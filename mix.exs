48e2e0797ea786fb1fb2fa3368ec7a1ed9f1f576
Quinlan Jager <quinlan.jager@gmail.com>
2019-01-26 15:39:58 -0800

    Add plug_cowboy to applications

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule BattleSnake2019.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :plug_cowboy],
       mod: {BattleSnake2019.Application, []}
     ]
   end

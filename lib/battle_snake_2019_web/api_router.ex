bd1eeb1cf951240872249d55e59b8260507b5b11
Quinlan Jager <quinlan.jager@gmail.com>
2019-01-27 14:45:26 -0800

    Add start and move routes with tests

--- lib/battle_snake_2019_web/api_router.ex
+++ lib/battle_snake_2019_web/api_router.ex
@@ -1,13 +1,23 @@
 defmodule BattleSnake2019.Web.APIRouter do
   use Plug.Router
   import Poison
+  alias BattleSnake2019.Snake
 
   plug(Plug.Parsers, parsers: [:json], pass: ["application/json"], json_decoder: Poison)
   plug(:match)
   plug(:dispatch)
 
-  post "/echo" do
-    body = conn.body_params()
-    send_resp(conn, 200, Poison.encode!(body))
+  post "/start" do
+    response = %{"color" => Snake.get_color()}
+    send_resp(conn, 200, encode!(response))
+  end
+
+  post "/move" do
+    body = conn.body_params
+    my_snake = body["you"]
+    other_snakes = body["board"]["snakes"]
+
+    response_body = encode!(Snake.move())
+    send_resp(conn, 200, response_body)
   end
 end
---
+++ test/battle_snake_2019_web/api_router_test.exs
@@ -0,0 +1,35 @@
+defmodule BattleSnake2019.Web.APIRouterTest do
+  use ExUnit.Case
+  use Plug.Test
+  alias BattleSnake2019.Snake
+  import Poison
+
+  test "it responds to a start request with a colour" do
+    conn = conn(:post, "/start") |> put_req_header("content-type", "application/json")
+    options = BattleSnake2019.Web.APIRouter.init([])
+
+    resp = BattleSnake2019.Web.APIRouter.call(conn, options)
+
+    assert resp.state == :sent
+    assert resp.status == 200
+
+    {:ok, decoded_body} = decode(resp.resp_body)
+
+    assert decoded_body == %{"color" => Snake.get_color()}
+  end
+
+  test "it responds to a move request with a move" do
+    valid_moves = Snake.get_valid_moves()
+    conn = conn(:post, "/move") |> put_req_header("content-type", "application/json")
+    options = BattleSnake2019.Web.APIRouter.init([])
+    resp = BattleSnake2019.Web.APIRouter.call(conn, options)
+
+    assert resp.state == :sent
+    assert resp.status == 200
+
+    {:ok, decoded_body} = decode(resp.resp_body)
+
+    assert %{"move" => move} = decoded_body
+    assert Enum.member?(valid_moves, move) == true
+  end
+end

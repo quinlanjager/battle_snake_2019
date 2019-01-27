103ffc9e7e01000b1bd551496469bd1b6c253287
Quinlan Jager <quinlan.jager@gmail.com>
2019-01-27 14:52:13 -0800

    Add end and ping routest with tests

--- test/battle_snake_2019_web/api_router_test.exs
+++ test/battle_snake_2019_web/api_router_test.exs
@@ -32,4 +32,22 @@ defmodule BattleSnake2019.Web.APIRouterTest do
     assert %{"move" => move} = decoded_body
     assert Enum.member?(valid_moves, move) == true
   end
+
+  test "it responds with a 200 when an end request is send" do
+    conn = conn(:post, "/end") |> put_req_header("content-type", "application/json")
+    options = BattleSnake2019.Web.APIRouter.init([])
+    resp = BattleSnake2019.Web.APIRouter.call(conn, options)
+
+    assert resp.state == :sent
+    assert resp.status == 200
+  end
+
+  test "it responds with a 200 when pinged" do
+    conn = conn(:post, "/ping") |> put_req_header("content-type", "application/json")
+    options = BattleSnake2019.Web.APIRouter.init([])
+    resp = BattleSnake2019.Web.APIRouter.call(conn, options)
+
+    assert resp.state == :sent
+    assert resp.status == 200
+  end
 end

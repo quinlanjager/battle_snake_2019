aec7242a3f9ca6316cd6ab016ff180a880b65bce
Quinlan Jager <quinlan.jager@gmail.com>
2019-02-14 23:47:17 -0800

    Improve pathsolving logic

--- lib/battle_snake_2019/pathsolver.ex
+++ lib/battle_snake_2019/pathsolver.ex
@@ -28,7 +28,7 @@ defmodule BattleSnake2019.Pathsolver do
 
     case paths_to_goal do
       {:ok, path, goal} ->
-        extended_path = Path.extend_path(path, goal)
+        extended_path = Path.extend_path(path, field)
         # index 0 is the start
         Waypoints.get_waypoint_direction(Enum.at(path, 1), start)
 
@@ -51,7 +51,7 @@ defmodule BattleSnake2019.Pathsolver do
             solve_path_to_goal(field, start, goal, unvisited_list, visited_list, path)
         end,
         ordered: false,
-        timeout: 200,
+        timeout: 500,
         on_timeout: :kill_task
       )
       |> Enum.filter(fn {message, _result} -> message != :error and message != :exit end)
--- lib/battle_snake_2019/pathsolver/path.ex
+++ lib/battle_snake_2019/pathsolver/path.ex
@@ -12,7 +12,7 @@ defmodule BattleSnake2019.Pathsolver.Path do
     end
   end
 
-  defp extend_path(path, field) do
+  def extend_path(path, field) do
     Enum.chunk_every(path, 2)
     |> Enum.map(fn paths ->
       extend_pair(paths, field)
@@ -21,7 +21,9 @@ defmodule BattleSnake2019.Pathsolver.Path do
   end
 
   defp extend_pair([path1, path2] = pair, field) do
-    direction = get_waypoint_direction(path2, path1)
+    IO.inspect(path1)
+    IO.inspect(path2)
+    direction = Waypoints.get_waypoint_direction(path2, path1)
 
     directions_to_check =
       if direction == "left" or direction == "right" do
@@ -33,12 +35,14 @@ defmodule BattleSnake2019.Pathsolver.Path do
     pairs_to_add =
       Enum.map(directions_to_check, fn direction_to_check ->
         Enum.map(pair, fn path ->
+          IO.inspect(path)
           Nodes.get_adjacent_node(field, path, direction_to_check)
         end)
       end)
       |> Enum.filter(fn [new_path_1, new_path_2] = set ->
         !Kernel.is_nil(new_path_1) and !Kernel.is_nil(new_path_2) and
-          Waypoints.keep_waypoint?(new_path_1, []) and Waypoints.keep_waypoint?(new_path_2, [])
+          Waypoints.keep_waypoint?(new_path_1, [], %{}) and
+          Waypoints.keep_waypoint?(new_path_2, [], %{})
       end)
 
     Enum.intersperse(pair, Enum.at(pairs_to_add, 0, []))

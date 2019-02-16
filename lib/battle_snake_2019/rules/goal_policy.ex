b2ba8b546dee3f811e2f0cb5e9204009331d6710
Quinlan Jager <quinlan.jager@gmail.com>
2019-02-16 10:29:46 -0800

    Adjust policies

--- lib/battle_snake_2019/rules/goal_policy.ex
+++ lib/battle_snake_2019/rules/goal_policy.ex
@@ -2,20 +2,21 @@ defmodule BattleSnake2019.Rules.GoalPolicy do
   use BattleSnake2019.Rules.Policy
 
   policy :safe_food do
-    weight_by(:body_size, :subtract, 3)
+    weight_by(:body_size, :subtract, 6)
     weight_by(:health_lost, :subtract, 0.25)
-    weight_by(:safe_food_length, :add, 3)
-    weight_by(:nearest_safe_food_dist, :subtract, 4)
+    weight_by(:safe_food_length, :add, 5)
+    weight_by(:nearest_safe_food_dist, :subtract, 1)
   end
 
   policy :all_food do
-    weight_by(:body_size, :subtract, 2)
+    weight_by(:body_size, :subtract, 6)
     weight_by(:health_lost, :subtract, 1)
     weight_by(:all_food_length, :add, 2)
     weight_by(:nearest_food_dist, :subtract, 1)
   end
 
   policy :tail do
+    # TODO add nearest enemy snake
     weight_by(:tail_safety, :subtract, 10)
     weight(:add)
   end

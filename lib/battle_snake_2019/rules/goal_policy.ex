08729a4e8e29f7af98086154c1f13a74006f013a
Quinlan Jager <quinlan.jager@gmail.com>
2019-02-16 23:43:15 -0800

    Add policy to hunt enemy

--- lib/battle_snake_2019/rules/goal_policy.ex
+++ lib/battle_snake_2019/rules/goal_policy.ex
@@ -1,6 +1,11 @@
 defmodule BattleSnake2019.Rules.GoalPolicy do
   use BattleSnake2019.Rules.Policy
 
+  policy :enemy_head do
+    weight_by(:enemy_head_distance, :subtract, 10)
+    weight_by(:enemy_body_difference, :add, 15)
+  end
+
   policy :safe_food do
     weight_by(:body_size, :subtract, 6)
     weight_by(:health_lost, :subtract, 0.25)
@@ -17,6 +22,8 @@ defmodule BattleSnake2019.Rules.GoalPolicy do
 
   policy :tail do
     # TODO add nearest enemy snake
+    weight_by(:enemy_head_distance, :add)
+    weight_by(:enemy_body_difference, :add, 5)
     weight_by(:tail_is_hidden, :subtract, 200)
     weight_by(:tail_safety, :subtract, 10)
     weight(:add)

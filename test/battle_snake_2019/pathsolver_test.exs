41b461f97a7ea58385147b9a0c6780cc0d0dfb83
Quinlan Jager <quinlan.jager@gmail.com>
2019-02-19 20:26:18 -0800

    update tests

--- test/battle_snake_2019/pathsolver_test.exs
+++ test/battle_snake_2019/pathsolver_test.exs
@@ -8,26 +8,29 @@ defmodule BattleSnake2019.PathsolverTest do
         "x" => 1,
         "y" => 1,
         entity: "snake-id-string",
-        segment_type: :tail
+        segment_type: :tail,
+        id: "1_1"
       },
       %{
         "x" => 1,
         "y" => 2,
         entity: "snake-id-string",
-        segment_type: :body
+        segment_type: :body,
+        id: "1_2"
       },
       %{
         "x" => 1,
         "y" => 3,
         entity: "snake-id-string",
-        segment_type: :head
+        segment_type: :head,
+        id: "1_3"
       },
-      %{"x" => 2, "y" => 1},
-      %{"x" => 2, "y" => 2},
-      %{"x" => 2, "y" => 3, entity: :food, segment_type: nil},
-      %{"x" => 3, "y" => 1},
-      %{"x" => 3, "y" => 2},
-      %{"x" => 3, "y" => 3}
+      %{"x" => 2, "y" => 1, id: "2_1"},
+      %{"x" => 2, "y" => 2, id: "2_2"},
+      %{"x" => 2, "y" => 3, entity: :food, segment_type: nil, id: "2_3"},
+      %{"x" => 3, "y" => 1, id: "3_1"},
+      %{"x" => 3, "y" => 2, id: "3_2"},
+      %{"x" => 3, "y" => 3, id: "3_3"}
     ]
 
     game = Map.put(example_game(), "field", field)
@@ -42,24 +45,27 @@ defmodule BattleSnake2019.PathsolverTest do
         "x" => 1,
         "y" => 1,
         entity: :food,
-        segment_type: nil
+        segment_type: nil,
+        id: "1_1"
       },
       %{
         "x" => 1,
-        "y" => 2
+        "y" => 2,
+        id: "1_2"
       },
       %{
         "x" => 1,
         "y" => 3,
         entity: "snake-id-string",
-        segment_type: :tail
+        segment_type: :tail,
+        id: "1_3"
       },
-      %{"x" => 2, "y" => 1},
-      %{"x" => 2, "y" => 2, entity: "snake-id-string", segment_type: :body},
-      %{"x" => 2, "y" => 3, entity: :food, segment_type: nil},
-      %{"x" => 3, "y" => 1},
-      %{"x" => 3, "y" => 2, entity: "snake-id-string", segment_type: :body},
-      %{"x" => 3, "y" => 3, entity: "snake-id-string", segment_type: :head}
+      %{"x" => 2, "y" => 1, id: "2_1"},
+      %{"x" => 2, "y" => 2, entity: "snake-id-string", segment_type: :body, id: "2_2"},
+      %{"x" => 2, "y" => 3, entity: :food, segment_type: nil, id: "2_3"},
+      %{"x" => 3, "y" => 1, id: "3_1"},
+      %{"x" => 3, "y" => 2, entity: "snake-id-string", segment_type: :body, id: "3_2"},
+      %{"x" => 3, "y" => 3, entity: "snake-id-string", segment_type: :head, id: "3_3"}
     ]
 
     game = Map.put(example_game(), "field", field)
@@ -74,24 +80,27 @@ defmodule BattleSnake2019.PathsolverTest do
         "x" => 1,
         "y" => 1,
         entity: :food,
-        segment_type: nil
+        segment_type: nil,
+        id: "1_1"
       },
       %{
         "x" => 1,
-        "y" => 2
+        "y" => 2,
+        id: "1_2"
       },
       %{
         "x" => 1,
         "y" => 3,
         entity: "snake-id-string",
-        segment_type: :tail
+        segment_type: :tail,
+        id: "1_3"
       },
-      %{"x" => 2, "y" => 1},
-      %{"x" => 2, "y" => 2, entity: "snake-id-string", segment_type: :body},
-      %{"x" => 2, "y" => 3, entity: :food, segment_type: nil},
-      %{"x" => 3, "y" => 1},
-      %{"x" => 3, "y" => 2, entity: "snake-id-string", segment_type: :body},
-      %{"x" => 3, "y" => 3, entity: "snake-id-string", segment_type: :head}
+      %{"x" => 2, "y" => 1, id: "2_1"},
+      %{"x" => 2, "y" => 2, entity: "snake-id-string", segment_type: :body, id: "2_2"},
+      %{"x" => 2, "y" => 3, entity: :food, segment_type: nil, id: "2_3"},
+      %{"x" => 3, "y" => 1, id: "3_1"},
+      %{"x" => 3, "y" => 2, entity: "snake-id-string", segment_type: :body, id: "3_2"},
+      %{"x" => 3, "y" => 3, entity: "snake-id-string", segment_type: :head, id: "3_3"}
     ]
 
     game = Map.put(example_game(), "field", field)

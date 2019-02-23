d607f56786079490a192b23e742492c5127bb4bc
Quinlan Jager <quinlan.jager@gmail.com>
2019-02-22 22:55:47 -0800

    Add Island gathering module

---
+++ lib/battle_snake_2019/islands.ex
@@ -0,0 +1,73 @@
+defmodule BattleSnake2019.Islands do
+  alias BattleSnake2019.Field.Nodes
+  alias BattleSnake2019.Pathsolver.Waypoints
+
+  def discover(field, primary_snake_id \\ nil) do
+    islands = []
+
+    gather_islands(field, islands, primary_snake_id)
+  end
+
+  def gather_islands(
+        [node | rest_of_field],
+        islands,
+        primary_snake_id
+      ) do
+    if is_map(node) and Waypoints.keep_waypoint?(node) do
+      new_island = build_island(rest_of_field, [node], [], primary_snake_id)
+
+      filtered_field =
+        Enum.filter(rest_of_field, fn %{id: id} ->
+          !Enum.any?(new_island, fn %{id: island_node_id} -> id == island_node_id end)
+        end)
+
+      gather_islands(
+        filtered_field,
+        islands ++ [new_island],
+        primary_snake_id
+      )
+    else
+      gather_islands(rest_of_field, islands, primary_snake_id)
+    end
+  end
+
+  def gather_islands(
+        [],
+        islands,
+        _primary_snake_id
+      ),
+      do: islands
+
+  def build_island(field, [], new_island, _primary_snake_id), do: new_island
+
+  def build_island(field, seed_nodes, new_island, primary_snake_id) do
+    update_island = new_island ++ seed_nodes
+
+    new_seed_nodes =
+      Enum.map(seed_nodes, fn node ->
+        Nodes.get_adjacent_nodes(field, node)
+        |> Enum.filter(fn seed_node ->
+          keep_node?(seed_node, new_island, primary_snake_id)
+        end)
+      end)
+      |> List.flatten()
+      |> Enum.uniq_by(fn %{id: id} -> id end)
+
+    build_island(field, new_seed_nodes, update_island, primary_snake_id)
+  end
+
+  def keep_node?(nil, _new_island, _primary_snake_id), do: false
+
+  def keep_node?(node, new_island, primary_snake_id) do
+    entity = Map.get(node, :entity)
+    segment = Map.get(node, :segment_type)
+
+    its_a_good_node =
+      Waypoints.keep_waypoint?(node) or (entity == primary_snake_id and segment == :head)
+
+    !is_in_island?(new_island, node) and its_a_good_node
+  end
+
+  def is_in_island?(island, %{id: id}),
+    do: Enum.any?(island, fn %{id: node_id} -> node_id == id end)
+end
---
+++ test/battle_snake_2019/islands_test.exs
@@ -0,0 +1,99 @@
+defmodule BattleSnake2019.IslandsTest do
+  use ExUnit.Case
+  alias BattleSnake2019.Islands
+  alias BattleSnake2019.Field
+
+  test "generates islands from field" do
+    #      0 1 2 3 4
+    #    0 x 1 x x x
+    #    1 x 1 1 1 x
+    #    2 x 2 2 2 x
+    #    3 2 2 x x x
+    #    4 x x x x x
+
+    snakes = [
+      %{
+        "id" => "1",
+        "body" => [
+          %{"x" => 3, "y" => 1},
+          %{"x" => 2, "y" => 1},
+          %{"x" => 1, "y" => 1},
+          %{"x" => 1, "y" => 0}
+        ]
+      },
+      %{
+        "id" => "2",
+        "body" => [
+          %{"x" => 3, "y" => 2},
+          %{"x" => 2, "y" => 2},
+          %{"x" => 1, "y" => 2},
+          %{"x" => 1, "y" => 3},
+          %{"x" => 0, "y" => 3}
+        ]
+      }
+    ]
+
+    islands = [
+      [
+        %{:id => "0_0", "x" => 0, "y" => 0},
+        %{
+          :dist => 2.23606797749979,
+          :entity => "1",
+          :id => "1_0",
+          :segment_type => :tail,
+          "x" => 1,
+          "y" => 0
+        },
+        %{:id => "0_1", "x" => 0, "y" => 1},
+        %{:id => "2_0", "x" => 2, "y" => 0},
+        %{:id => "0_2", "x" => 0, "y" => 2},
+        %{:id => "3_0", "x" => 3, "y" => 0},
+        %{
+          :dist => 3.605551275463989,
+          :entity => "2",
+          :id => "0_3",
+          :segment_type => :tail,
+          "x" => 0,
+          "y" => 3
+        },
+        %{:id => "4_0", "x" => 4, "y" => 0},
+        %{
+          :dist => 0.0,
+          :entity => "1",
+          :id => "3_1",
+          :segment_type => :head,
+          "x" => 3,
+          "y" => 1
+        },
+        %{:id => "0_4", "x" => 0, "y" => 4},
+        %{:id => "4_1", "x" => 4, "y" => 1},
+        %{:id => "1_4", "x" => 1, "y" => 4},
+        %{:id => "4_2", "x" => 4, "y" => 2},
+        %{:id => "2_4", "x" => 2, "y" => 4},
+        %{:id => "4_3", "x" => 4, "y" => 3},
+        %{:id => "3_4", "x" => 3, "y" => 4},
+        %{:id => "2_3", "x" => 2, "y" => 3},
+        %{:id => "3_3", "x" => 3, "y" => 3},
+        %{:id => "4_4", "x" => 4, "y" => 4}
+      ]
+    ]
+
+    game = %{
+      "board" => %{
+        "snakes" => snakes,
+        "food" => [],
+        "height" => 5,
+        "width" => 5
+      },
+      "you" => Enum.find(snakes, fn %{"id" => id} -> id == "1" end)
+    }
+
+    field =
+      Field.create_field(game)
+      |> Field.update_field(game)
+
+    island_result = Islands.discover(field, "1")
+
+    assert island_result == islands
+  end
+end

8d2829e764ad0824e36e21f6514d7e2179bf9551
Quinlan Jager <quinlan.jager@gmail.com>
2019-02-27 21:13:32 -0800

    Update test result

--- test/battle_snake_2019/rules/goal_policy_test.exs
+++ test/battle_snake_2019/rules/goal_policy_test.exs
@@ -6,6 +6,6 @@ defmodule BattleSnake2019.Rules.GoalPolicyTest do
   test "evaluates to the food goal spec!" do
     facts = %{health: 1}
     result = Judge.evaluate(facts, GoalPolicy)
-    assert result == [tail: 1, all_food: 0, safe_food: 0, enemy_head: 0]
+    assert result == [tail: 1, all_food: 0, ok_food: 0, safe_food: 0.0, enemy_head: 0]
   end
 end
